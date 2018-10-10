ea995c7fbb083cb563b986320c8eb6ef41c623fc
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-10-10 14:30:17 -0700

    updated table with column name and data in seeds

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -9,9 +9,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {email: 'aj@gmail.com', password: 'ajspassword'},
-        {email: 'johndoe@gmail.com', password: 'johnpassword'},
-        {email: 'janedoe@gmail.com', password: 'janepassword'}
+        {name: 'aj', email: 'aj@gmail.com', password: 'ajspassword'},
+        {name: 'john doe', email: 'johndoe@gmail.com', password: 'johnpassword'},
+        {name: 'jane doe', email: 'janedoe@gmail.com', password: 'janepassword'}
       ]);
     });
 };
