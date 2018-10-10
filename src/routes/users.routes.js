a25693d3d5352ba583bdb202d3541068f25d1868
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-10-10 13:09:53 -0700

    created functions in for user controllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/users.controller');
 
-//users table routes
 router.get('/', ctrl.fetchUsers)
 router.get('/:id', ctrl.fetchUser)
 
