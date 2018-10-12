7bb6c60d477782626813b6fbcc505b3048e6b552
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-10-12 13:31:40 -0700

    installed cors package

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
 //==================================================================
 
 //==================================================================
